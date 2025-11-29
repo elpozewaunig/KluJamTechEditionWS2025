c966c8687256dba82dd94933359bf63c59ce5979
XtophB <xtophb@gmail.com>
2025-11-29 18:18:45 +0100

    HUD clamping is now dynamic, but panning speed is fixed

--- scenes/texture_rect.gd
+++ scenes/texture_rect.gd
@@ -1,13 +1,17 @@
 extends TextureRect
 
 var return_speed = 10
-var pan_speed = 1000
+var pan_speed = 200
+var texture_height = size.y
+var texture_height_half = size.y / 2
+var min_panning = -texture_height + 500
+var max_panning = 200
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	var viewport_height = get_viewport_rect().size.y
+	var center_y_pos = (viewport_height / 2.0) - (texture_height / 2.0)
+	position.y = center_y_pos+200
+	# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pan_camera(delta)
 	
@@ -19,5 +23,7 @@ func pan_camera(delta):
 	if Input.is_action_pressed("TiltUp"):
 		position.y += pan_speed * delta
 		is_input_active = true
+	position.y = clamp(position.y, min_panning, max_panning)
+	
 	if not is_input_active:
-		position.y = lerp(position.y, 0.0, return_speed * delta)
+		position.y = lerp(position.y, -texture_height_half, return_speed * delta)
